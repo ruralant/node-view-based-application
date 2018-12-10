17a8b979dcfedb3d28c973ee77b56d43d6e06860
Antonio Rossi <info@antoniorossi.net>
2018-12-10 22:17:10 +0000

    edit product page implemented

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,26 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false,
+  });
+};
+
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) return res.redirect('/');
+  const { id } = req.params;
+  Product.findById(id, product => {
+    if (!product) return res.redirect('/');
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    });
   });
 };
 
@@ -15,6 +29,10 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.postEditProduct = (req, res, next) => {
+  
 };
 
 exports.getProducts = (req, res, next) => {
