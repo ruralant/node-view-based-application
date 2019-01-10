856318bca317818d64a49806df276c4f8ebfcde9
Antonio Rossi <info@antoniorossi.net>
2019-01-10 22:34:02 +0000

    committed unsaved file

--- controllers/admin.js
+++ controllers/admin.js
@@ -54,7 +54,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
