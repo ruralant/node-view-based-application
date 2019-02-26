c12775df809f0f36140064edd78735ad3af37657
Antonio Rossi <antoniorossi@MacBook-Pro-di-Antonio.local>
2019-02-26 22:04:51 +0000

    password reset logic and user authorisation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
@@ -101,3 +102,107 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  message.length > 0 ? message = message[0] : message = null;
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+}
+
+exports.postReset = (req, res, next) => {
+  const {
+    email
+  } = req.body;
+  crypto.randomBytes(32, async (e, buffer) => {
+    try {
+      if (e) return res.redirect('/reset')
+
+      const token = buffer.toString('hex');
+      const user = await User.findOne({
+        email
+      });
+      if (!user) {
+        req.flash('error', 'No user found with the provided email');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExp = Date.now() + 3600000;
+      await user.save();
+      res.redirect('/');
+      transporter.sendMail({
+        to: email,
+        from: process.env.EMAIL_SENDER,
+        subject: 'Password reset',
+        html: `
+          <p>You requested a password reset</p>
+          <p>Click <a href="${process.env.HOSTNAME}/reset/${token}">HERE</a> to reset the password.</p>
+          <p>This link will expire in 1 hour.</p>
+        `
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
+
+exports.getNewPassword = async (req, res, next) => {
+  const {
+    token
+  } = req.params;
+  try {
+    const user = await User.findOne({
+      resetToken: token,
+      resetTokenExp: {
+        $gt: Date.now()
+      }
+    });
+    if (!user) {
+      req.flash('error', 'The reset password token is invalid');
+      return res.redirect('/reset');
+    }
+    let message = req.flash('error');
+    message.length > 0 ? message = message[0] : message = null;
+    res.render('auth/new-password', {
+      path: '/new-password',
+      pageTitle: 'Update Password',
+      errorMessage: message,
+      userId: user._id.toString(),
+      token
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+exports.postNewPassword = async (req, res, next) => {
+  const {
+    userId,
+    password,
+    token
+  } = req.body;
+  try {
+    const user = await User.findOne({
+      _id: userId,
+      resetToken: token,
+      resetTokenExp: {
+        $gt: Date.now()
+      }
+    });
+    if (!user) {
+      req.flash('error', 'The reset password token is invalid');
+      return res.redirect('/');
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    user.password = hashedPassword;
+    user.resetToken = undefined;
+    user.resetTokenExp = undefined;
+    await user.save();
+    return res.redirect('/login');
+  } catch (e) {
+    console.log(e);
+  }
+}
--- models/user.js
+++ models/user.js
@@ -11,6 +11,8 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+  resetToken: String,
+  resetTokenExp: Date,
   cart: {
     items: [{
       productId: {
